34b2b6ad56b131cc6d024ec5e141ecbf601d817c
Mikael Johansson <mikael.johansson@meltwater.com>
2015-05-20 14:41:45 +0200

    Exit nicely on signals but avoid PID 1 zombie reaping problem

--- proxymatic.sh
+++ proxymatic.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-# Shutdown nicely on exit signals 
-function finally {
-	exit 0
-}
-trap finally SIGINT SIGHUP SIGTERM
-
 # Run Python script in subprocess to make sure that [defunct] processes are reaped 
 # properly by bash. Google "docker PID 1 zombie reaping problem" for more info.
-exec python /usr/lib/python/proxymatic/main.py $@
+python /usr/lib/python/proxymatic/main.py $@ & wait
